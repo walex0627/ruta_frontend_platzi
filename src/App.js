50b578160733b822acbcd1f4066a87ba2ed8bafd
walex0627 <walteralex0627@gmail.com>
2026-01-07 13:28:09 -0500

    3. Componentes de ToDO machine

--- src/App.js
+++ src/App.js
@@ -1,48 +1,21 @@
 import logo from './platzi.webp';
 import './App.css';
-
+import {TodoCount, TodoItem, TodoSearch, TodoList, CreateTodoButton} from './components/Todo';
 function App() {
   return (
     <div className="App">
       <TodoCount/>
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoSearch/>
+
+      <TodoList>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+      </TodoList>
+
+      <CreateTodoButton/>
     </div>
   );
-}
-
-
-function TodoItem(){
-  return(
-    <li>
-      <span>V</span>
-      <p>Aprender React</p>
-      <span>X</span>
-    </li>
-  );
-}
-
-function TodoCount(){
-    return(
-        <h1>Todo Count
-            <h4>3 de 5 completados</h4>
-        </h1>
-    )
 }
 
 
