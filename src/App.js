085827653236853438fde7e11f0392b97981d126
walex0627 <walteralex0627@gmail.com>
2026-01-07 10:47:23 -0500

    2. Que es un componente?

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <TodoCount/>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,5 +25,25 @@ function App() {
     </div>
   );
 }
+
+
+function TodoItem(){
+  return(
+    <li>
+      <span>V</span>
+      <p>Aprender React</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+function TodoCount(){
+    return(
+        <h1>Todo Count
+            <h4>3 de 5 completados</h4>
+        </h1>
+    )
+}
+
 
 export default App;
