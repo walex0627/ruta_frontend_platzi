d173e1517f4b0c87016a8bc4a95ef216ad4dc723
walex0627 <walteralex0627@gmail.com>
2026-01-08 16:44:43 -0500

    11. Iconos en react

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,18 @@
 import '../styles/TodoItem.css'
+import CheckIcon from '@mui/icons-material/Check';
+import DeleteIcon from '@mui/icons-material/Delete';
 function TodoItem(props) {
   return (
       <li className='TodoItem'>
+        <CheckIcon onClick={props.onComplete} className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}/>
+        {/* Lo dejo comentado pq ando probando material ui
         <span onClick= {props.onComplete} className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>v 
             {props.completed}
-            </span>
+            </span> */}
         <p className={`TodoItem-p  ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
-        <span onClick={props.onDelete} className='Icon Icon-delete'>X</span>
+        <DeleteIcon onClick={props.onDelete} className='Icon Icon-delete'/>
+        {/* Lo dejo comentado pq ando probando material ui
+        <span onClick={props.onDelete} className='Icon Icon-delete'>X</span> */}
       </li>
   );
 }
