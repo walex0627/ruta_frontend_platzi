c7b667590e3c8971c1310d58aa0227e7d72fc91a
walex0627 <walteralex0627@gmail.com>
2026-01-08 07:43:30 -0500

    6. Eventos onClick y onChange

--- src/components/CreateTodoButton.js
+++ src/components/CreateTodoButton.js
@@ -2,7 +2,10 @@ import '../styles/CreateTodoButton.css'
 
 function CreateTodoButton(){
   return (
-    <button className="createTodoButton">Crear Tarea</button>
+    <button className="createTodoButton" onClick={(event)=>{console.log('le diste click')
+      console.log(event)
+      console.log(event.target)
+    }}>Crear Tarea</button>
   )
 }
 export { CreateTodoButton };
--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,7 +1,12 @@
 import '../styles/TodoSearch.css'
 function TodoSearch() {
   return (
-    <input placeholder="Cortar cebolla" className="TodoSearch"></input>
+    <input placeholder="Cortar cebolla" className="TodoSearch"
+    onChange={(event)=>{
+      console.log(event);
+      console.log(event.target.value);
+      console.log('estas escribiendo en el TodoSerch');
+    }}></input>
   )
 }
 
